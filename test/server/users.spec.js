d57189ce4dfe47a881ff423a46471f1d2ba990ea
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-04 22:44:48 +0300

    Set up Mocha

---
+++ test/server/users.spec.js
@@ -0,0 +1,8 @@
+var should = require('should');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      should(5).be.exactly(5).and.be.a.Number();
+    });
+  });
+});
