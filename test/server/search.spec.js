e347dc001910e4203464740d4b413f10cff34b5c
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-04 23:12:59 +0300

    All specs working

---
+++ test/server/search.spec.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  var should = require('should');
+  describe('Search', function() {
+    describe('#indexOf(search)', function () {
+      it('should return -1 when the value is not present', function () {
+        should(5).be.exactly(5).and.be.a.Number();
+      });
+    });
+  });
+})();
