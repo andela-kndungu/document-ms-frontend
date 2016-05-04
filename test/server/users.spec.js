e01c5796026467e19cd0e5d9c67ffab375cbb52f
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-04 23:02:52 +0300

    Set up login helpers

--- test/server/users.spec.js
+++ test/server/users.spec.js
@@ -1,8 +1,12 @@
-var should = require('should');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      should(5).be.exactly(5).and.be.a.Number();
+(function() {
+  'use strict';
+
+  var should = require('should');
+  describe('Array', function() {
+    describe('#indexOf()', function () {
+      it('should return -1 when the value is not present', function () {
+        should(5).be.exactly(5).and.be.a.Number();
+      });
     });
   });
-});
+})();
