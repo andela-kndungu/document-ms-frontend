4023ac6d6dd432750fed3ab77fb301f42b844d59
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-05 10:38:26 +0300

    All roles tests pass

--- server/router/routes/roles.js
+++ server/router/routes/roles.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
-var RolesController = require('../../controllers/users');
+var RolesController = require('../../controllers/roles');
 
 // e.g. GET localhost:8080/roles
 router.get('/', RolesController.getAll);
 
-// // e.g. POST localhost:8080/users/13
-// router.get('/:id', RolesController.getById);
+// e.g. POST localhost:8080/roles
+router.post('/', RolesController.addRole);
 
 module.exports = router;
