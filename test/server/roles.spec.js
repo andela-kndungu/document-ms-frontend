c440c37cc21ad13060a814a8eeb6df3bb4ba910d
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-05 09:53:54 +0300

    Add roles tests

--- test/server/roles.spec.js
+++ test/server/roles.spec.js
@@ -1,11 +1,43 @@
 (function() {
   'use strict';
 
+  var app = require('../../app');
   var should = require('should');
+  var request = require('supertest');
+  var loginHelper = require('./helpers/login');
+  var seeder = require('./helpers/seeder');
+  var adminToken;
   describe('Roles', function() {
-    describe('#indexOf(roles)', function () {
-      it('should return -1 when the value is not present', function () {
-        should(5).be.exactly(5).and.be.a.Number();
+    describe('Returns all roles', function() {
+      it('responds with an array of all roles', function(done) {
+        request(app)
+          .get('/users')
+          .set('x-access-token', adminToken)
+          .set('Accept', 'application/json')
+          .end(function(error, res) {
+            should.not.exist(error);
+            res.body.should.be.an.Array;
+            should(res.body.length).be.exactly(2);
+            done();
+          });
+      });
+    });
+    describe('Creates a new role', function() {
+      it('successfully creates a new role', function(done) {
+        request(app)
+          .post('/users')
+          .set('x-access-token', adminToken)
+          .send({
+            title: 'viewer'
+          })
+          .set('Accept', 'application/json')
+          .end(function(error, res) {
+            should.not.exist(error);
+            res.status.should.equal(200);
+            res.body.success.should.equal(true);
+            (res.body.message).should.containEql('created successfully');
+            done();
+          });
       });
     });
   });
