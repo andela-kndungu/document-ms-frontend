7f012905e4b2f251518e35f399b52a49e0a742b4
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-05-09 22:09:16 +0300

    Move token authentication function to controllers folder

--- server/router/index.js
+++ server/router/index.js
@@ -1,57 +1,22 @@
 (function() {
   'use strict';
-
-  // To process token
-  var jwt = require('jsonwebtoken');
-
   // Get the routes
-  var publicRoutes = require('./routes/public');
-  var usersRoutes = require('./routes/users');
-  var rolesRoutes = require('./routes/roles');
-  var categoriesRoutes = require('./routes/categories');
-  var documentsRoutes = require('./routes/documents');
-  var searchRoutes = require('./routes/search');
+  var publicRoutes = require('./routes/public'),
+   usersRoutes = require('./routes/users'),
+   rolesRoutes = require('./routes/roles'),
+   categoriesRoutes = require('./routes/categories'),
+   documentsRoutes = require('./routes/documents'),
+   searchRoutes = require('./routes/search');
 
-  // Middleware to protect sensitive routes
-  var authenticateUser = function(req, res, next) {
-    // Get token in the request header
-    var token = req.headers['x-access-token'];
-
-    // If there is a token
-    if (token) {
-      // Decode token with the secret key
-      jwt.verify(token, process.env.SECRET_KEY, function(error, decoded) {
-        // Perhaps a forged token
-        if (error) {
-          return res.status(403).send({
-            success: false,
-            message: 'Failed to authenticate token'
-          });
-        } else {
-          // If it checks out save in request object for others to use
-          req.decoded = decoded;
-
-          // Pass it over to the next function
-          next();
-        }
-      });
-
-    } else {
-      // No token provided
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
-
-    }
-  };
+  // Authentication Middleware
+  var authenticate = require('../controllers/authenticate');
 
   module.exports = function(app) {
-    // Use routes above based on route visited by user
+    // Accessible without being logged in
     app.use('/', publicRoutes);
 
     // Protect sensitive routes
-    app.use(authenticateUser);
+    app.use(authenticate.token);
     app.use('/users', usersRoutes);
     app.use('/roles', rolesRoutes);
     app.use('/categories', categoriesRoutes);
