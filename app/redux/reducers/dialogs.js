97c116319c954bbab8005bacec5b662f2a7bc177
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-08-23 10:36:17 +0300

    [Feature #128833581] write log in dialog tests

--- app/redux/reducers/dialogs.js
+++ app/redux/reducers/dialogs.js
@@ -1,19 +1,23 @@
 import { fromJS } from 'immutable';
 import constants from '../constants';
 
+// At first all dialogs are not visiible
 const defaultState = fromJS({
   dialogs: {
     logInOpen: false
   }
 });
 
+// If state is undefined use default state
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case constants.TOGGLE_LOG_IN:
+      // Negate the current value, i.e. if open close
       return state.updateIn(['dialogs', 'logInOpen'], (currentValue) => {
         return !currentValue;
       });
     default:
+      // Always return current state as the default
       return state;
   }
 };
---
+++ test/client/actions/toggleLogIn.spec.js
@@ -0,0 +1,10 @@
+import { toggleLogInDialog } from '../../../app/redux/actions';
+import constants from '../../../app/redux/constants';
+
+describe('Actions :: toggleLogInDialog', () => {
+  it('Returns expected object when called', () => {
+    toggleLogInDialog((action) => {
+      (action.type).should.eql(constants.TOGGLE_LOG_IN);
+    });
+  });
+});
