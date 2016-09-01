af980fbcc13ff4537002861a4caddb72eee1846a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-09-01 14:23:36 +0300

    [Chore #129601807] Migrate helpers/login to es6

--- test/server/helpers/login.js
+++ test/server/helpers/login.js
@@ -1,59 +1,56 @@
-(function() {
-  'use strict';
+import request from 'supertest';
+import app from '../../../server';
 
-  var app = require('../../../server'),
-    request = require('supertest');
+module.exports = {
+  admin: (callback) => {
+    request(app)
+      .post('/api/users/login')
+      .send({
+        username: 'admin',
+        password: 'adminPassword'
+      })
+      .set('Accept', 'application/json')
+      .end((error, res) => {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, res);
+        }
+      });
+  },
 
-  module.exports = {
-    admin: function(callback) {
-      request(app)
-        .post('/api/users/login')
-        .send({
-          username: 'admin',
-          password: 'adminPassword'
-        })
-        .set('Accept', 'application/json')
-        .end(function(error, res) {
-          if (error) {
-            callback(error);
-          } else {
-            callback(null, res);
-          }
-        });
-    },
+  user: (callback) => {
+    request(app)
+      .post('/api/users/login')
+      .send({
+        username: 'user',
+        password: 'userPassword'
+      })
+      .set('Accept', 'application/json')
+      .end((error, res) => {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, res);
+        }
+      });
+  },
 
-    user: function(callback) {
-      request(app)
-        .post('/api/users/login')
-        .send({
-          username: 'user',
-          password: 'userPassword'
-        })
-        .set('Accept', 'application/json')
-        .end(function(error, res) {
-          if (error) {
-            callback(error);
-          } else {
-            callback(null, res);
-          }
-        });
-    },
+  unauthorized: (callback) => {
+    request(app)
+      .post('/api/users/login')
+      .send({
+        username: 'unauthorized',
+        password: 'unauthorizedPassword'
+      })
+      .set('Accept', 'application/json')
+      .end((error, res) => {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, res);
+        }
+      });
+  }
+};
 
-    unauthorized: function(callback) {
-      request(app)
-        .post('/api/users/login')
-        .send({
-          username: 'unauthorized',
-          password: 'unauthorizedPassword'
-        })
-        .set('Accept', 'application/json')
-        .end(function(error, res) {
-          if (error) {
-            callback(error);
-          } else {
-            callback(null, res);
-          }
-        });
-    }
-  };
-})();
